18d082f5e7011baa0166a5b141f11cae4495a47d
anushawsu <151598603+anushawsu@users.noreply.github.com>
2023-11-20 21:53:41 -0600

    created index.html and path to home page

--- main.py
+++ main.py
@@ -70,6 +70,22 @@ with app.app_context():
     cursor.close()
     conn.close()
 
+# Home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+
+
+
+
+
+
+
     # Run the application
 if __name__ == '__main__':
     app.run(debug=True)

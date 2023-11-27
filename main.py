0a62d23178df0a49254d34ca38d15c2b27a7fcdf
anushawsu <151598603+anushawsu@users.noreply.github.com>
2023-11-27 15:31:08 -0600

    Added library_members homepage and path

--- main.py
+++ main.py
@@ -203,7 +203,184 @@ def delete_author(author_id):
     # Redirect to the authors index page
     return redirect('/authors_index')
 
+<<<<<<< HEAD
+# Define a route for displaying all library members
+@app.route('/library_members_index', methods=['GET', 'POST'])
+def library_members_index():
+    # Connect to the database
+    conn = get_db()
+    cursor = conn.cursor()
 
+    if request.method == 'POST':
+        # If a search form is submitted
+        search_query = request.form.get('search')
+        cursor.execute('''
+            SELECT * FROM LibraryMember
+            WHERE MemberID LIKE ? OR FirstName LIKE ? OR LastName LIKE ? OR Address LIKE ? OR Mobile LIKE ? OR Email LIKE ?
+        ''', ('%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%'))
+        members = cursor.fetchall()
+    else:
+        # Retrieve all library members from the LibraryMember table
+        cursor.execute('SELECT * FROM LibraryMember')
+        members = cursor.fetchall()
+
+    # Close the cursor and database connection
+    cursor.close()
+    conn.close()
+
+    # Render the template with the list of library members
+    return render_template('library_members_index.html', members=members)
+
+=======
+# Home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+>>>>>>> d804056fdc4e69f496873084cd8e571ae8fb6f28
+
+# Route for displaying all books
+@app.route('/book_index', methods=['GET', 'POST'])
+def book_index():
+    # Connect to the database
+    conn = get_db()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+        # If a search form is submitted
+        search_query = request.form.get('search')
+        cursor.execute('''
+            SELECT Book.ISBN, Book.Title, Author.AuthorName, Book.Edition, Book.PublishedYear, Book.BookLocation
+            FROM Book
+            INNER JOIN Author ON Book.Author = Author.AuthorID
+            WHERE Book.Title LIKE ? OR Author.AuthorName LIKE ? OR Book.BookLocation LIKE ?
+        ''', ('%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%'))
+        books = cursor.fetchall()
+    else:
+        # Retrieve all books from the Book table with author names
+        cursor.execute('''
+            SELECT Book.ISBN, Book.Title, Author.AuthorName, Book.Edition, Book.PublishedYear, Book.BookLocation
+            FROM Book
+            INNER JOIN Author ON Book.Author = Author.AuthorID
+        ''')
+        books = cursor.fetchall()
+
+    # Close the cursor and database connection
+    cursor.close()
+    conn.close()
+
+    # Render the template with the list of books
+    return render_template('book_index.html', books=books)
+
+# Add_book route
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        # Get form data
+        title = request.form.get('title')
+        author_id = request.form.get('author')  # Use author_id instead of author name
+        edition = request.form.get('edition')
+        published_year = request.form.get('published_year')
+        book_location = request.form.get('book_location')
+
+        # Generate a new ISBN
+        isbn = generate_isbn()
+
+        # Connect to the database
+        conn = get_db()
+        cursor = conn.cursor()
+
+        # Insert a new book into the Book table
+        cursor.execute('''
+            INSERT INTO Book (ISBN, Title, Author, Edition, PublishedYear, BookLocation)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ''', (isbn, title, author_id, edition, published_year, book_location))
+
+        # Commit the changes and close the cursor and database connection
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        # Redirect to the book index page
+        return redirect('/book_index')
+    else:
+        # Fetch authors for the dropdown
+        conn = get_db()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM Author')
+        authors = cursor.fetchall()
+        cursor.close()
+        conn.close()
+
+        return render_template('add_book.html', authors=authors)
+
+def generate_isbn():
+    # Generate a random 10-digit number as the ISBN
+    return str(random.randint(1000000000, 9999999999))
+
+# Update the route for rendering the update book page
+@app.route('/book_update/<int:isbn>', methods=['GET', 'POST'])
+def book_update(isbn):
+    # Connect to the database
+    conn = get_db()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+        # Get form data
+        title = request.form.get('title')
+        author = request.form.get('author')
+        edition = request.form.get('edition')
+        published_year = request.form.get('published_year')
+        book_location = request.form.get('book_location')
+
+        # Update the book in the Book table based on the given ISBN
+        cursor.execute('''
+            UPDATE Book
+            SET Title = ?, Author = ?, Edition = ?, PublishedYear = ?, BookLocation = ?
+            WHERE ISBN = ?
+        ''', (title, author, edition, published_year, book_location, isbn))
+
+        # Commit the changes and close the cursor and database connection
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        # Redirect to the book index page after updating
+        return redirect('/book_index')
+    else:
+        # Retrieve existing book details for the specified ISBN
+        cursor.execute('SELECT * FROM Book WHERE ISBN = ?', (isbn,))
+        book_details = cursor.fetchone()
+        cursor.execute('SELECT AuthorName FROM Author WHERE AuthorID = ?', (book_details[2],))
+        authorname = cursor.fetchone()[0]
+
+        # Close the cursor and database connection
+        cursor.close()
+        conn.close()
+
+        if book_details:
+            # Render the update_book.html template with the existing book details
+            return render_template('update_book.html', book=book_details, aname=authorname)
+        else:
+            # Handle case where ISBN is not found
+            return "Book not found."
+        
+        # Define a route for deleting a book
+@app.route('/book_delete/<int:isbn>', methods=['POST'])
+def delete_book(isbn):
+    # Connect to the database
+    conn = get_db()
+    cursor = conn.cursor()
+
+    # Delete the book from the Book table based on the given ISBN
+    cursor.execute('DELETE FROM Book WHERE ISBN = ?', (isbn,))
+
+    # Commit the changes and close the cursor and database connection
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    # Redirect to the book index page
+    return redirect('/book_index')
 
 
 

1fd7af30e6f8da1bf024443f27b252919adec2f4
anushawsu <151598603+anushawsu@users.noreply.github.com>
2023-11-27 15:33:51 -0600

    added library members path

--- main.py
+++ main.py
@@ -203,10 +203,33 @@ def delete_author(author_id):
     # Redirect to the authors index page
     return redirect('/authors_index')
 
-# Home page
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Define a route for displaying all library members
+@app.route('/library_members_index', methods=['GET', 'POST'])
+def library_members_index():
+    # Connect to the database
+    conn = get_db()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+        # If a search form is submitted
+        search_query = request.form.get('search')
+        cursor.execute('''
+            SELECT * FROM LibraryMember
+            WHERE MemberID LIKE ? OR FirstName LIKE ? OR LastName LIKE ? OR Address LIKE ? OR Mobile LIKE ? OR Email LIKE ?
+        ''', ('%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%'))
+        members = cursor.fetchall()
+    else:
+        # Retrieve all library members from the LibraryMember table
+        cursor.execute('SELECT * FROM LibraryMember')
+        members = cursor.fetchall()
+
+    # Close the cursor and database connection
+    cursor.close()
+    conn.close()
+
+    # Render the template with the list of library members
+    return render_template('library_members_index.html', members=members)
+
 
 # Route for displaying all books
 @app.route('/book_index', methods=['GET', 'POST'])
